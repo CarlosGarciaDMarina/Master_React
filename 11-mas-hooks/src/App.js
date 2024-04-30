e600a6ea40912db5d342e31d6e20edc412112789
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-05-01 00:31:11 +0200

    Formulario de prueba controlando los distintos estados

--- 11-mas-hooks/src/App.js
+++ 11-mas-hooks/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { MiComponente } from './components/MiComponente';
 import { PruebasCustom } from './components/PruebasCustom';
+import { MiFormulario } from './components/MiFormulario';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <PruebasCustom />
+        <MiFormulario />
+        
       </header>
     </div>
   );
